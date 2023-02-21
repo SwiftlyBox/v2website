b009b534f272298288190eb27aa7e7af8f250d09
spiderwebd <123647409+spiderwebd@users.noreply.github.com>
2023-02-21 11:09:08 +0200

    kill

--- pages/assets.js
+++ pages/assets.js
@@ -1,30 +1,11 @@
 import React from "react"
-import 27 from "../public/assets/27.jpg";
-import 26 from "../public/assets/26.jpg";
-import 25 from "../public/assets/25.jpg";
-import 24 from "../public/assets/24.jpg";
-import 23 from "../public/assets/22.jpg";
-import 22 from "../public/assets/22.jpg";
-import 21 from "../public/assets/21.jpg";
-import 10 from "../public/assets/10.jpg";
-import 11 from "../public/assets/11.jpg";
-import 12 from "../public/assets/12.jpg";
-import 13 from "../public/assets/13.jpg";
-import 14 from "../public/assets/14.jpg";
-import 15 from "../public/assets/15.jpg";
-import 16 from "../public/assets/16.jpg";
+
 
 
 const Assets =()=> {
     return (
       <>
-        <Image
-          src={15}
-          alt="image1"
-          width="500"
-          height="20"
-          style={{ borderRadius: 20 }}
-        />
+        
       </>
     );
 }
